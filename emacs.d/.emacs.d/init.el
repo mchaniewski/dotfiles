9dc9f48e301b61dbee3955635b10f5f02f18c50f
Mariusz Chaniewski <mariusz.chaniewski@gmail.com>
2015-03-07 23:46:26 +0000

    ELPA Package system.

--- emacs.d/.emacs.d/init.el
+++ emacs.d/.emacs.d/init.el
@@ -1,3 +1,15 @@
+;; ELPA (Emacs Lisp Package Archive)
+;; Following configuration required only for
+;; Emacs below version 24.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/package.el")))
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "https://marmalade-repo.org/packages/"))
+(package-initialize)
+
 ;; Load paths.
 (add-to-list 'load-path "~/.emacs.d/custom")
 
