67a7bed29c36dc8ff1a672f492b0a061172ee1f2
Mariusz Chaniewski <mariusz.chaniewski@gmail.com>
2015-03-07 23:57:26 +0000

    Extracted ELPA setup to separate file. Renamed and moved package.el to custom directory.

--- emacs.d/.emacs.d/init.el
+++ emacs.d/.emacs.d/init.el
@@ -1,17 +1,6 @@
-;; ELPA (Emacs Lisp Package Archive)
-;; Following configuration required only for
-;; Emacs below version 24.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/package.el")))
-(require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "https://marmalade-repo.org/packages/"))
-(package-initialize)
-
 ;; Load paths.
 (add-to-list 'load-path "~/.emacs.d/custom")
 
 ;; Load custom configuration.
-(load "00setup.el")
+(load "elpa.el")
+(load "setup.el")
